116e5e622e989424511462df0c3dcd101bc4ea74
AGGELOS KORALIS <sdi2300080@linux08.di.uoa.gr>
2024-02-27 00:15:24 +0200

    it actually almost works but it still missed some lines

--- 2023/day1/day1b.c
+++ 2023/day1/day1b.c
@@ -43,55 +43,53 @@ int nine(char *line, int length, int i) {
     return (length >= i + 3) && (line[i] == 'n' && line[i + 1] == 'i' && line[i + 2] == 'n' && line[i + 3] == 'e');
 }
 
-//the starting index will help figure out which to put first or last    i think
+//return the number that it found adnd -1 if it didnt find any
 int find_words(char *line, int starting_idx) {
 
     int length = strlen(line);
     int i = starting_idx;
-    //while i dont bump into a number loop
-    //while (!(line[i] - '0' >= 0 && line[i] - '0' <= 9)) { //no loop? the loop is in the caller func
 
-        if (one(line, length, i)) {
-            return 1;
+    if (one(line, length, i)) {
+        return 1;
+    }
+    else if (line[i] == 't') {
+        if (two(line, length, i)) {
+            return 2;
         }
-        else if (line[i] == 't') {
-            if (two(line, length, i)) {
-                return 2;
-            }
-            else if (three(line, length, i)) {
-                return 3;
-            }
+        else if (three(line, length, i)) {
+            return 3;
         }
-        else if (line[i] == 'f') {
-            if (four(line, length, i)) {
-                return 4;
-            }
-            else if (five(line, length, i)) {
-                return 5;
-            }
+    }
+    else if (line[i] == 'f') {
+        if (four(line, length, i)) {
+            return 4;
         }
-        else if (line[i] == 's') {
-            if (six(line, length, i)) {
-                return 6;
-            }
-            else if (seven(line, length, i)) {
-                return 7;
-            }
-        }  
-        else if (eight(line, length, i)) {
-            return 8;
+        else if (five(line, length, i)) {
+            return 5;
         }
-        else if (nine(line, length, i)) {
-            return 9;
+    }
+    else if (line[i] == 's') {
+        if (six(line, length, i)) {
+            return 6;
         }
+        else if (seven(line, length, i)) {
+            return 7;
+        }
+    }  
+    else if (eight(line, length, i)) {
+        return 8;
+    }
+    else if (nine(line, length, i)) {
+        return 9;
+    }
 
-    //}
     return -1; //no word found
 }
 
 int find_nums(char *line) {
     int first_num, last_num;
     int first_num_found = 0;
+    int fn_idx = -1, ln_idx = -1; //indices of first/last nums
 
     int first_word, last_word;
     int first_word_found = 0;
@@ -103,29 +101,49 @@ int find_nums(char *line) {
             if (!first_num_found) {
                 first_num_found = 1;
                 first_num = line[i] - '0';
+                fn_idx = i;
                 last_num = line[i] - '0';
+                ln_idx = i;
             }
             else {
                 last_num = line[i] - '0';
+                ln_idx = i;
             }
         }
         else if (isalpha(line[i])) {
             if (!first_word_found) {
-                first_word = find_words(line, i);
-                last_word = first_word;
                 first_word_found = 1;
+                first_word = find_words(line, i);
+                fw_idx = i;
+                last_word = first_word;
+                lw_idx = i;
             }
             else {
                 if ((temp = find_words(line, i)) != -1) { //because if it didnt find a word lsat_word would get -1 from find_words()
                     last_word = temp;
+                    lw_idx = i;
                 }
             }
         }
-
-
     }
+    printf("fw: %d (idx: %d)     lw: %d (idx: %d)\n", first_word, fw_idx, last_word, lw_idx);
 
-    return 10 * first_num + last_num;
+    int first, last;
+
+    //find which one to keep as first
+    if (fn_idx == -1 || fn_idx >= fw_idx)
+        first = first_word;
+    else if (fw_idx == -1 || fn_idx < fw_idx) 
+        first = first_num;
+    
+    //find which one to keep as last
+    if (ln_idx == -1 || ln_idx <= lw_idx)
+        last = last_word;
+    else if (lw_idx == -1 || ln_idx > lw_idx) 
+        last = last_num;
+
+    printf("final first: %d   final last: %d\n", first, last);
+    return 10 * first + last;
 }
 
 int main(int argc, char **argv) {
