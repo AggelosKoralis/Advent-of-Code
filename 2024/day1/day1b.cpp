a4e1cde64b3c8b89958ee7eee07f874fbfcd93f6
AggelosKoralis <angelos.koralis@gmail.com>
2024-12-01 13:38:12 +0200

    ok

--- 2024/day1/day1b.cpp
+++ 2024/day1/day1b.cpp
@@ -19,10 +19,30 @@ int main(void) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
     
-    int sum = 0;
-    for (i = 0; i < a.size(); i++)
-        sum += abs(a[i] - b[i]);
+    map<int, int> mapA, mapB;
 
+    for (i = 0; i < a.size(); i++) {
+        mapA[a[i]] = 0;
+        
+        mapB[b[i]] = 0;
+    }
+   
+
+    for (i = 0; i < a.size(); i++) {
+        mapA[a[i]]++;
+        mapB[b[i]]++;
+    }
+    
+
+    int similarity = 0;
+    int sum = 0;
+    for (i = 0; i < a.size(); i++) {
+        sum += abs(a[i] - b[i]);
+        similarity += mapB[a[i]] * a[i];
+    }
+    
     cout << "distance: " << sum << endl;
+    cout << "similarity: " << similarity << endl;
+
     return 0;
 }
